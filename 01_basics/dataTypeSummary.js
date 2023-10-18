d219e747e446c2b164e466c5c5b6546dd6d2a37d
NiteshDivergent <148222737+NiteshDivergent@users.noreply.github.com>
2023-10-18 09:42:42 +0000

    Data Type Summary

---
+++ 01_basics/dataTypeSummary.js
@@ -0,0 +1,28 @@
+/* 
+As we know we have 7 premetive dataTypes
+string,
+boolean,
+number,
+undefine,
+Bigint,
+symbol
+*/
+
+
+
+
+/*
+Refrance type or non primitive data types:
+1. Arrays: const heroes=["Ram","Laxman","Bharat", "Satrughan"];
+
+2. Objects:present in scope with key and value paires 
+let myObj={
+    nam = "Nitesh"  inside the scope present objects
+    age = 23 
+}
+
+3. Functions 
+const myFunction=function (){
+console.log("Welcome to New Funtion")
+}
+*/
