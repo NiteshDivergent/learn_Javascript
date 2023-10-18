7a3c5fafe672d360afb0d56cb7903c93907c7266
NiteshDivergent <148222737+NiteshDivergent@users.noreply.github.com>
2023-10-18 05:44:31 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" // it will be working as javascript new version
+
+let num = 99; // its a numeric value like int its limit from 2 to power 54
+
+let big = 999999999999999; //its very high value its veryu rearly using in the project when we have large value.
+
+let na ="Nitesh"; //its a String type
+let n = null; // its also the String type but here we are not define the string value here in javascript null is the object
+
+let bo =true; // its boolean type which can contain only true and false value only
+
+let un; // its an undefine value when we are not defining the value so its undefine
+
+let x =Symbol("Divergent") // Symbol its containing the unique values only
+
+console.table([typeof num,typeof big,typeof na, typeof n, typeof bo, typeof un,typeof x]);
