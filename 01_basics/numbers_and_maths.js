0526d9fcebb78642aeac6358d3be4b9582c19922
NiteshDivergent <148222737+NiteshDivergent@users.noreply.github.com>
2023-10-18 11:51:07 +0000

    numbers and Math

---
+++ 01_basics/numbers_and_maths.js
@@ -0,0 +1,26 @@
+const num = 500;
+//console.log(num);
+
+const modifiyedNum = new Number(400);
+// console.log(modifiyedNum);
+// console.log(modifiyedNum.toString());
+// console.log(modifiyedNum.toFixed(3));
+
+const money = 123.87659
+//console.log(money.toPrecision(3));
+
+//++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(5,3,2,6));
+// console.log(Math.max(5,3,2,6));
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);
+
+const min= 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(min-max+1))+min);
