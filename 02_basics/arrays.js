5557db35136359fdf1952e2778acf5acd29eecda
NiteshDivergent <148222737+NiteshDivergent@users.noreply.github.com>
2023-10-19 05:49:54 +0000

    Arrays basics Only

---
+++ 02_basics/arrays.js
@@ -0,0 +1,15 @@
+const arr = [1,2,3,4,56,6,6,67,78];
+const arr1 = ["sachin Patel" , "Sachin Rathod"];
+const arr2 = new Array(1,3,5,7,9)
+console.table([arr,arr1,arr2]);
+
+/*
+
+here arrays are dynamic mean its length can be change. its not fixed 
+push method help us to push the element 
+pop method use to remove the last element from the array
+indextOf , include so many oprations present in Array
+slice : its does not mainuplate the array 
+splice : mainuplate the array 
+
+*/
